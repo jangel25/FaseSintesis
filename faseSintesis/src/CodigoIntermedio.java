d9d4efe369b9c38d702045793aa43fb408ec7536
jangel25 <angel251095@gmail.com>
2018-07-19 19:58:09 -0600

    m

---
+++ faseSintesis/src/CodigoIntermedio.java
@@ -0,0 +1,172 @@
+
+import java.util.ArrayList;
+
+public class CodigoIntermedio {
+    ArrayList<String> salida;
+    ArrayList<Token> entrada;
+    int cid,cserv,cdis,csol,csen,clig;
+    
+    public CodigoIntermedio(ArrayList<Token> e){
+        this.entrada=e;
+        this.salida=new ArrayList<>();
+    }
+    
+    public void reinicia(){
+        cid=cserv=cdis=csol=csen=clig=1;
+    }
+    public void imprime(){
+        for(int i=0;i<salida.size();i++){
+            System.out.println(salida.get(i));
+        }
+    }
+    
+    public void genCod(){
+        int i=0;
+        reinicia();
+        String aux="";
+        
+        while(i<entrada.size()){
+            if(entrada.get(i).equals("servo")){
+                aux+="sv"+cserv+"=";
+                cserv++;
+                while(!(entrada.get(i).equals("(")))
+                    i++;
+                i++;
+                while(!(entrada.get(i).equals(")"))){
+                    aux+=entrada.get(i);
+                    i++;
+                }
+                aux+=";";
+                salida.add(aux);
+                aux="";
+            }
+            if(entrada.get(i).equals("solenoid")){
+                aux+="so"+csol+"=";
+                csol++;
+                while(!(entrada.get(i).equals("(")))
+                    i++;
+                i++;
+                while(!(entrada.get(i).equals(")"))){
+                    aux+=entrada.get(i);
+                    i++;
+                }
+                aux+=";";
+                salida.add(aux);
+                aux="";
+            }
+            if(entrada.get(i).equals("display")){
+                aux+="di"+cdis+"=";
+                cdis++;
+                while(!(entrada.get(i).equals("(")))
+                    i++;
+                i++;
+                while(!(entrada.get(i).equals(")"))){
+                    aux+=entrada.get(i);
+                    i++;
+                }
+                aux+=";";
+                salida.add(aux);
+                aux="";
+            }
+            if(entrada.get(i).equals("sensor")){
+                aux+="se"+csen+"=";
+                csen++;
+                while(!(entrada.get(i).equals("(")))
+                    i++;
+                i++;
+                while(!(entrada.get(i).equals(")"))){
+                    aux+=entrada.get(i);
+                    i++;
+                }
+                aux+=";";
+                salida.add(aux);
+                aux="";
+            }
+            if(entrada.get(i).equals("ligth")){
+                aux+="li"+clig+"=";
+                clig++;
+                while(!(entrada.get(i).equals("(")))
+                    i++;
+                i++;
+                while(!(entrada.get(i).equals(")"))){
+                    aux+=entrada.get(i);
+                    i++;
+                }
+                aux+=";";
+                salida.add(aux);
+                aux="";
+            }
+            if(entrada.get(i).equals("int")||entrada.get(i).equals("text")
+               ||entrada.get(i).equals("boolean")||entrada.get(i).equals("float")){
+                aux+="t"+cid+"=";
+                cid++;
+                while(!(entrada.get(i).equals("=")))
+                    i++;
+                i++;
+                while(!(entrada.get(i).equals("$"))){
+                    aux+=entrada.get(i);
+                    i++;
+                }
+                aux+=";";
+                salida.add(aux);
+                aux="";
+            }
+            if(entrada.get(i).equals("task")){
+                reinicia();
+            }
+            
+            
+            i++;
+        }     
+        
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<String> prueba=new ArrayList<>();
+        prueba.add("task");
+        prueba.add("{");
+        prueba.add("int");
+        prueba.add("hola");
+        prueba.add("=");
+        prueba.add("23");
+        prueba.add("$");
+        prueba.add("text");
+        prueba.add("hola");
+        prueba.add("=");
+        prueba.add("23");
+        prueba.add("$");
+        prueba.add("}");
+        prueba.add("task");
+        prueba.add("{");
+        prueba.add("servo");
+        prueba.add("(");
+        prueba.add("8");
+        prueba.add(")");
+        prueba.add("$");
+        prueba.add("}");
+        prueba.add("task");
+        prueba.add("{");
+        prueba.add("servo");
+        prueba.add("(");
+        prueba.add("8");
+        prueba.add(")");
+        prueba.add("$");
+        prueba.add("}");
+        prueba.add("task");
+        prueba.add("{");
+        prueba.add("solenoid");
+        prueba.add("(");
+        prueba.add("14");
+        prueba.add(")");
+        prueba.add("$");
+        prueba.add("solenoid");
+        prueba.add("(");
+        prueba.add("80");
+        prueba.add(")");
+        prueba.add("$");
+        prueba.add("}");
+        CodigoIntermedio ci=new CodigoIntermedio(prueba);
+        ci.genCod();
+        ci.imprime();
+    }
+}
---
+++ faseSintesis/src/Token.java
@@ -0,0 +1,9 @@
+public class Token{
+   String valor;
+   int linea;
+   
+   public Token(String _valor, int _linea){
+      this.valor=_valor;
+      this.linea=_linea;
+   }
+}
